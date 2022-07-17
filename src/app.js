ca78b0ed79668d521b5ef85ff2793efcbc61525e
SisterHyde <juliacagle@gmail.com>
2022-07-17 15:24:57 -0500

    Added current date and time

--- src/app.js
+++ src/app.js
@@ -1,4 +1,35 @@
-//FETCHING WEATHER FROM API
+function getDate() {
+  let now = new Date();
+  let currentDay = now.getDay();
+  let currentHour = now.getHours();
+  let currentMinutes = now.getMinutes();
+
+  //ADDING A ZERO IF MINUTES ARE UNDER 10
+  function getFullMinutes() {
+    if (currentMinutes < 10) {
+      return `0${currentMinutes}`;
+    } else {
+      return `${currentMinutes}`;
+    }
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  currentDay = days[currentDay];
+
+  let updateTime = document.querySelector("#current-date");
+  updateTime.innerHTML = `${currentDay}, ${currentHour}:${getFullMinutes(
+    currentMinutes
+  )}`;
+  console.log(updateTime);
+}
 
 function getTemperature(response) {
   console.log(response.data);
@@ -16,6 +47,9 @@ function getTemperature(response) {
 
   let windElement = document.querySelector("#current-wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+
+  let dateElement = document.querySelector("#current-date");
+  getDate();
 }
 
 let apiKey = "87b9e142701e346ddd9ec0db3824a563";
